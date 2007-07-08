28799151ebdf8fb451758a2d1c194352aa09d7e2
tapajos <tapajos@ebe10805-befa-409d-9f47-33f710be3117>
2007-07-08 20:24:22 +0000

    Adicionando regras de plural.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@25 ebe10805-befa-409d-9f47-33f710be3117

--- trunk/lib/active_record_portuguese.rb
+++ trunk/lib/active_record_portuguese.rb
@@ -1,18 +1,18 @@
 module ActiveRecord
   class Errors
     @@default_error_messages = {
-      :inclusion => "n„o est· incluÌdo na lista",
-      :exclusion => "est· reservado",
-      :invalid => "È inv·lido.",
-      :confirmation => "n„o corresponde ‡ confirmaÁ„o",
+      :inclusion => "n√£o est√° inclu√≠do na lista",
+      :exclusion => "est√° reservado",
+      :invalid => "√© inv√°lido.",
+      :confirmation => "n√£o corresponde √† confirma√ß√£o",
       :accepted => "deve ser aceito",
-      :empty => "n„o pode estar vazio",
-      :blank => "n„o pode estar em branco",
-      :too_long => "muito longo (m·ximo %d caracteres)",
-      :too_short => "muito curto (m·nimo %d caracteres)",
+      :empty => "n√£o pode estar vazio",
+      :blank => "n√£o pode estar em branco",
+      :too_long => "muito longo (m√°ximo %d caracteres)",
+      :too_short => "muito curto (m√≠nimo %d caracteres)",
       :wrong_length => "de comprimento errado (deveria ter %d caracteres)",
-      :taken => "j· est· em uso",
-      :not_a_number => "n„o È um n˙mero"
+      :taken => "j√° est√° em uso",
+      :not_a_number => "n√£o √© um n√∫mero"
     }
   end
 end
---
+++ trunk/test/inflector_test.rb
@@ -0,0 +1,94 @@
+require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + "/../lib/inflector_portuguese")
+
+class InflectorTest < Test::Unit::TestCase
+  
+  #general rule: add "s" to the end of the word
+  def test_general_rule
+    words = {:casa => "casas",
+             :pe => "pes",
+             :saci => "sacis",
+             :carro => "carros",
+             :pneu => "pneus"
+            }
+    verify_pluralize words
+    verify_singularize words
+  end
+
+  #if word ends in "r" or "z", add "es"
+  def test_when_word_ends_in_r_or_z
+    words = {:flor => "flores",
+             :luz => "luzes"
+            }
+    verify_pluralize words
+    verify_singularize words
+  end
+  
+  #if word ends in "al", "el", "ol", "ul": trade "l" with "is"
+  def test_when_word_ends_in_al_el_ol_ul
+      words = {:hospital => "hospitais",
+               :telemovel => "telemoveis",
+               :farol => "farois",
+               :azul => "azuis"
+            }
+    verify_pluralize words
+    verify_singularize words
+  end
+  
+  
+  #if word ends in "il" and has tônic accent in last syllable, trade "il" with "is"
+  def test_when_word_end_in_il
+    words = {:cantil => "cantis"}
+    verify_pluralize words
+  end
+
+
+  #if word ends in "m", trade "m" with "ns"
+  def test_plurilize_when_word_ends_in_m
+    words = {:armazem => "armazens"}
+    verify_pluralize words
+    verify_singularize words
+  end
+  
+  #if word ends in "s" and has one silable, trade "s" with "es"
+  def test_plurilize_when_word_ends_in_s
+    words = {:portugues => "portugueses"}
+    verify_pluralize words
+  end
+  
+  def test_when_word_ends_in_ao
+    words = {:portão => "portões"}
+    verify_pluralize words
+    verify_singularize words
+  end
+  
+  def test_when_irregular_singular
+    words = {:cão => "cães",
+             :pão => "pães",
+             :mão => "mãos",
+             :alemão => "alemães"
+            }
+    verify_singularize words
+  end
+  
+  
+  def test_when_uncountable
+    words = {:tennis => "tennis",
+             :torax => "torax"
+            }
+    verify_pluralize words
+    verify_singularize words
+  end
+  
+  
+  private
+  
+  def verify_pluralize(words)
+    words.each { |key,value| assert_equal value, key.to_s.pluralize}    
+  end
+  
+  def verify_singularize(words)
+    words.each { |key,value| assert_equal key.to_s, value.singularize}    
+  end
+
+end
