5a7a90e166ea5ab6b98b5bbe53ba1bdb1b3ac870
Marcos Tapajós <tapajos@improveit.com.br>
2007-08-01 01:44:41 +0000

    Traduzindo os nomes dos meses e dias.

--- trunk/lib/date_portuguese.rb
+++ trunk/lib/date_portuguese.rb
@@ -10,6 +10,14 @@ end
 
 class Date
 
+	old_verbose = $VERBOSE
+	$VERBOSE = nil
+	MONTHNAMES = [nil] + %w(Janeiro Fevereiro Marco Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)
+  DAYNAMES = %w(Domingo Segunda-Feira Terca-Feira Quarta-Feira Quinta-Feira Sexta-Feira Sabado)
+  ABBR_MONTHNAMES = [nil] + %w(Jan Fev Mar Abr Mai Jun Jul Ago Set Out Nov Dez)
+  ABBR_DAYNAMES = %w(Dom Seg Ter Qua Qui Sex Sab)
+	$VERBOSE = old_verbose
+
   def to_s(locale = nil)
     return send("to_s_#{locale.downcase}") if locale && locale.downcase == "br"
     strftime
