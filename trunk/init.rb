8ff2d217fbaf3fc5d6c30529215d23e042919480
tapajos <tapajos@ebe10805-befa-409d-9f47-33f710be3117>
2007-09-03 12:12:38 +0000

    Adiconando dinheiro.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@47 ebe10805-befa-409d-9f47-33f710be3117

--- trunk/init.rb
+++ trunk/init.rb
@@ -1,4 +1,16 @@
 require 'active_record_portuguese'
 require 'action_view_portuguese'
 require 'date_portuguese'
+require 'dinheiro_util'
+require 'excecoes'
+
 Time.send(:include, DateUtils)
+
+class Numeric
+  include DinheiroUtil
+end
+
+class String
+  include DinheiroUtil
+end
+
