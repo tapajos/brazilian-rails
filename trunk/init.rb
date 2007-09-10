7c89977b7418c072a9192f6157cfb0bce72e4816
Marcos Tapajós <tapajos@improveit.com.br>
2007-09-10 15:01:04 +0000

    Ajustando o init.

--- trunk/init.rb
+++ trunk/init.rb
@@ -1,16 +1,10 @@
 require 'active_record_portuguese'
 require 'action_view_portuguese'
 require 'date_portuguese'
+require 'dinheiro'
 require 'dinheiro_util'
 require 'excecoes'
 
 Time.send(:include, DateUtils)
-
-class Numeric
-  include DinheiroUtil
-end
-
-class String
-  include DinheiroUtil
-end
-
+Numeric.send(:include, DinheiroUtil)
+String.send(:include, DinheiroUtil)
