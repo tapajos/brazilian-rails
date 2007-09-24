75589c917814c8ea89c6139204dbdb3369f7b2f2
tapajos <tapajos@ebe10805-befa-409d-9f47-33f710be3117>
2007-09-24 19:16:03 +0000

    Agora o time tem o to_s_br
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@66 ebe10805-befa-409d-9f47-33f710be3117

--- lib/date_utils.rb
+++
@@ -1,7 +0,0 @@
-module DateUtils
-  
-  def to_s_br
-    strftime("%d/%m/%Y")
-  end
-  
-end
---
+++ test/time_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + "/../lib/time_portuguese")
+
+class TimeTest < Test::Unit::TestCase
+  
+  #to_s
+  def test_time_to_s_with_traditional_format
+    assert_equal "Mon Sep 24 16:03:05 UTC 2007", "Mon Sep 24 16:03:05 UTC 2007".to_time.to_s
+  end
+  
+  #to_s_br
+  def test_time_to_s_br
+    assert_equal "24/09/2007 16:03", "Mon Sep 24 16:03:05 UTC 2007".to_time.to_s_br
+  end
+  
+  def test_month_names
+    assert_equal [nil,
+      "Janeiro",
+      "Fevereiro",
+      "Marco",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro"],
+      Time::MONTHNAMES
+  end
+	
+  def test_days_names
+    assert_equal ["Domingo",
+      "Segunda-Feira",
+      "Terca-Feira",
+      "Quarta-Feira",
+      "Quinta-Feira",
+      "Sexta-Feira",
+      "Sabado"],
+      Time::DAYNAMES
+  end
+
+  def test_abbr_monthnames
+    assert_equal [nil,
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez"],
+      Time::ABBR_MONTHNAMES
+  end
+
+  def test_abbr_daysnames
+    assert_equal ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"], Time::ABBR_DAYNAMES
+  end
+
+end
