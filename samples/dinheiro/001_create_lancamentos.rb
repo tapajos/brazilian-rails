1648dc1f2c877645e46f894872e0a59c1a802995
viniciusteles <viniciusteles@ebe10805-befa-409d-9f47-33f710be3117>
2007-09-27 00:09:13 +0000

    Exemplos de utilizacao da classe dinheiro em uma classe de modelo.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@73 ebe10805-befa-409d-9f47-33f710be3117

---
+++ samples/dinheiro/001_create_lancamentos.rb
@@ -0,0 +1,12 @@
+class CreateLancamentos < ActiveRecord::Migration
+  def self.up
+    create_table :lancamentos do |t|
+      t.column :descricao,  :string,    :null => false
+      t.column :valor,      :decimal,   :precision => 14, :scale => 2, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :lancamentos
+  end
+end
