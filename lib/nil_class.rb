37a4df5c0deea82e64918f8aa89904f225ca97ff
tapajos <tapajos@ebe10805-befa-409d-9f47-33f710be3117>
2007-11-13 17:13:08 +0000

    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@91 ebe10805-befa-409d-9f47-33f710be3117

--- lib/nil_class.rb
+++ lib/nil_class.rb
@@ -1,23 +1,23 @@
 class NilClass
 
   def valor
-    ""
+    0.real
   end
 
-  def valor_contabil
-    ""
+  def contabil
+    0.contabil
   end
   
   def para_dinheiro
-    ""
+    0.real
   end
 
   def reais
-    ""
+    0.real
   end
 
   def real
-    ""
+    0.real
   end  
 
 end
