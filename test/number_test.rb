b1e7cf8e760232cc2c900449751132b5536de4c1
Celestino Gomes <tinorj@gmail.com>
2007-11-28 17:06:58 +0000

    Correção dos testes para rodarem no Windows.

--- test/number_test.rb
+++ test/number_test.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/test_helper')
+#require File.expand_path(File.dirname(__FILE__) + '/test_helper')
+require File.dirname(__FILE__) + '/test_helper'
 
 class NumberTest < Test::Unit::TestCase
   def test_unidades
