91718bc7bedd850b6722d1187a4fa84e4342d28e
tapajos <tapajos@ebe10805-befa-409d-9f47-33f710be3117>
2007-12-19 15:52:06 +0000

    melhorando o usar_como_dinheiro. acho que agora funciona melhor
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@101 ebe10805-befa-409d-9f47-33f710be3117

--- lib/dinheiro_active_record.rb
+++ lib/dinheiro_active_record.rb
@@ -6,14 +6,23 @@ module DinheiroActiveRecord#:nodoc:
     def usar_como_dinheiro(*args)#:nodoc:
       unless args.size.zero?
         args.each do |name|
-          composed_of name, :class_name => 'Dinheiro', :mapping => [name, "#{name.to_s}_decimal"], :allow_nil => true
+          composed_of name, :class_name => 'Dinheiro', :mapping => [name, "valor_decimal"], :allow_nil => false
           name = name.to_s
           module_eval <<-ADICIONANDO_METODO
-        alias_method :#{name}_original=, :#{name}=
-        
-        def #{name}= valor
-          self.#{name}_original = valor if valor.kind_of?(Dinheiro)
-          self.#{name}_original = valor.real unless valor.kind_of?(Dinheiro)
+          
+        def #{name}=(value)
+          if value.kind_of?(Dinheiro)
+            write_attribute(:#{name},value.valor_decimal)
+          else
+            begin
+              value = value.real
+              write_attribute(:#{name},value.valor_decimal)
+            rescue
+              self.valid?
+              self.errors.add(:#{name}, $!)
+            end
+          end
+          
         end
           ADICIONANDO_METODO
         end
