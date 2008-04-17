09b339bf2ee43af0851c0b26edc814faa059a2be
tinorj <tinorj@ebe10805-befa-409d-9f47-33f710be3117>
2008-04-17 23:39:57 +0000

    Novas mensagens de erros
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@125 ebe10805-befa-409d-9f47-33f710be3117

--- lib/active_record_portuguese.rb
+++ lib/active_record_portuguese.rb
@@ -3,19 +3,27 @@ module ActiveRecord
   class Errors
     @@default_error_messages = {
       :inclusion => "não está incluído na lista",
-      :exclusion => "está reservado",
-      :invalid => "é inválido.",
+      :exclusion => "é reservado",
+      :invalid => "é inválido",
       :confirmation => "não corresponde à confirmação",
-      :accepted => "deve ser aceito",
-      :empty => "não pode estar vazio",
-      :blank => "não pode estar em branco",
-      :too_long => "muito longo (máximo %d caracteres)",
-      :too_short => "muito curto (mínimo %d caracteres)",
-      :wrong_length => "de comprimento errado (deveria ter %d caracteres)",
+      :accepted  => "deve ser aceito",
+      :empty => "deve ser preenchido",
+      :blank => "deve ser preenchido",
+      :too_long => "deve ter até %d caractere(s)",
+      :too_short => "deve ter no mínimo %d caractere(s)",
+      :wrong_length => "deve ter %d caractere(s)",
       :taken => "já está em uso",
       :not_a_number => "não é um número",
-      :less_than_or_equal_to => "deve ser menor ou igual a %d"
+      :greater_than => "deve ser maior que %d",
+      :greater_than_or_equal_to => "deve ser maior ou igual a %d",
+      :equal_to => "deve ser igual a %d",
+      :less_than => "deve ser menor que %d",
+      :less_than_or_equal_to => "deve ser menor ou igual a %d",
+      :odd => "deve ser impar",
+      :even => "deve ser par"
     }
   end
   
 end
+
+
--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -10,22 +10,29 @@ end
 
 class ActiveRecordTest < Test::Unit::TestCase
   
-  # Replace this with your real tests.
   def test_this_plugin
     errors = ActiveRecordTestable.default_error_messages
     assert_equal "não está incluído na lista", errors[:inclusion]  
-    assert_equal "está reservado", errors[:exclusion]  
-    assert_equal "é inválido.", errors[:invalid]  
+    assert_equal "é reservado", errors[:exclusion]  
+    assert_equal "é inválido", errors[:invalid]  
     assert_equal "não corresponde à confirmação", errors[:confirmation]  
     assert_equal "deve ser aceito", errors[:accepted]  
-    assert_equal "não pode estar vazio", errors[:empty]  
-    assert_equal "não pode estar em branco", errors[:blank]  
-    assert_equal "muito longo (máximo %d caracteres)", errors[:too_long]  
-    assert_equal "muito curto (mínimo %d caracteres)", errors[:too_short]  
-    assert_equal "de comprimento errado (deveria ter %d caracteres)", errors[:wrong_length]  
+    assert_equal "deve ser preenchido", errors[:empty]  
+    assert_equal "deve ser preenchido", errors[:blank]  
+    assert_equal "deve ter até %d caractere(s)", errors[:too_long]  
+    assert_equal "deve ter no mínimo %d caractere(s)", errors[:too_short]  
+    assert_equal "deve ter %d caractere(s)", errors[:wrong_length]  
     assert_equal "já está em uso", errors[:taken]  
     assert_equal "não é um número", errors[:not_a_number]
+    assert_equal "deve ser maior que %d", errors[:greater_than]
+    assert_equal "deve ser maior ou igual a %d", errors[:greater_than_or_equal_to]
+    assert_equal "deve ser igual a %d", errors[:equal_to]
+    assert_equal "deve ser menor que %d", errors[:less_than]
     assert_equal "deve ser menor ou igual a %d", errors[:less_than_or_equal_to]  
+    assert_equal "deve ser impar", errors[:odd]
+    assert_equal "deve ser par", errors[:even]
   end
 
 end
+
+
