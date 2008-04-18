07a6816313f84b6144891f4edc5ddd955c2c2dee
Celestino Gomes <tinorj@gmail.com>
2008-04-18 00:20:57 +0000

    Refatoração nos testes de String e atualização da API

--- test/string_portuguese_test.rb
+++ test/string_portuguese_test.rb
@@ -18,6 +18,14 @@ NOMES_PROPRIOS = {
 }
 
 class StringPortugueseTest < Test::Unit::TestCase
+  def test_letras_maiusculas
+    assert_equal 'ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙÄËÏÖÜÃÕÑÇ', String::MAIUSCULAS
+  end
+
+  def test_letras_minusculas
+    assert_equal 'abcdefghijklmnopqrstuvwxyzáéíóúâêîôûàèìòùäëïöüãõñç', String::MINUSCULAS
+  end
+
   def test_nome_proprio
     NOMES_PROPRIOS.each {|key, value| assert_equal key, value.nome_proprio }
     
@@ -28,6 +36,22 @@ class StringPortugueseTest < Test::Unit::TestCase
     end
   end
   
+  def test_nome_proprio!
+    NOMES_PROPRIOS.each do |key, value| 
+      nome = value
+      nome.nome_proprio!
+      assert_equal key, nome
+    end
+    
+    palavras_excluidas = %w(? ! @ # $ % & * \ / ? , . ; ] [ } { = + 0 1 2 3 4 5 6 7 8 9)
+    
+    palavras_excluidas.each do |char|
+      nome = char.clone
+      nome.nome_proprio!
+      assert_equal char, nome, "Não deveria alterar o caracter '#{char}'"
+    end
+  end
+
   def test_remover_acentos
     assert_equal 'aeiouAEIOU', "áéíóúÁÉÍÓÚ".remover_acentos
     assert_equal 'aeiouAEIOU', "âêîôûÂÊÎÔÛ".remover_acentos
@@ -39,22 +63,28 @@ class StringPortugueseTest < Test::Unit::TestCase
     assert_equal 'aeiouAEIOUaeiouAEIOUaeiouAEIOUaeiouAEIOUaoAOnNcC', "áéíóúÁÉÍÓÚâêîôûÂÊÎÔÛàèìòùÀÈÌÒÙäëïöüÄËÏÖÜãõÃÕñÑçÇ".remover_acentos
   end
   
+  def test_remover_acentos!
+    string = 'áéíóúÁÉÍÓÚ'
+    string.remover_acentos!
+    assert_equal 'aeiouAEIOU', string
+  end
+
   def test_downcase
-    assert_equal String::MINUSCULAS_COM_ACENTO, String::MAIUSCULAS_COM_ACENTO.downcase
+    assert_equal String::MINUSCULAS, String::MAIUSCULAS.downcase
   end
   
   def test_upcase
-    assert_equal String::MAIUSCULAS_COM_ACENTO, String::MINUSCULAS_COM_ACENTO.upcase
+    assert_equal String::MAIUSCULAS, String::MINUSCULAS.upcase
   end
 
   def test_downcase_br
     RAILS_DEFAULT_LOGGER.expects(:warn).with("AVISO: Este metodo sera removido na proxima revisao, usar o metodo downcase.")
-    assert_equal String::MINUSCULAS_COM_ACENTO, String::MAIUSCULAS_COM_ACENTO.downcase_br
+    assert_equal String::MINUSCULAS, String::MAIUSCULAS.downcase_br
   end
   
   def test_upcase_br
     RAILS_DEFAULT_LOGGER.expects(:warn).with("AVISO: Este metodo sera removido na proxima revisao, usar o metodo upcase.")
-    assert_equal String::MAIUSCULAS_COM_ACENTO, String::MINUSCULAS_COM_ACENTO.upcase_br
+    assert_equal String::MAIUSCULAS, String::MINUSCULAS.upcase_br
   end
   
   def test_titleize
@@ -64,29 +94,18 @@ class StringPortugueseTest < Test::Unit::TestCase
     assert_equal ' José  Da   Silva  ', ' josé  da   silva  '.titleize
     assert_equal 'Átila Da Silva', 'átila da silva'.titleize
     assert_equal "Á É Í Ó Ú À È Ì Ò Ù Ã Õ Â Ê Î Ô Û Ä Ë Ï Ö Ü", 'á é í ó ú à è ì ò ù ã õ â ê î ô û ä ë ï ö ü'.titleize
-end
-
-  def test_nome_proprio!
-    string = 'joão Dos santos'
-    string.nome_proprio!
-    assert_equal 'João dos Santos', string
   end
 
-  def test_remover_acentos!
-    string = 'áéíóúÁÉÍÓÚ'
-    string.remover_acentos!
-    assert_equal 'aeiouAEIOU', string
-  end
 
   def test_upcase!
-    string = 'áéíóúâêîôûàèìòùäëïöüãõñç'
+    string = String::MINUSCULAS.clone
     string.upcase!
-    assert_equal 'ÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙÄËÏÖÜÃÕÑÇ', string
+    assert_equal String::MAIUSCULAS, string
   end
 
   def test_downcase!
-    string = 'ÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙÄËÏÖÜÃÕÑÇ'
+    string = String::MAIUSCULAS.clone
     string.downcase!
-    assert_equal 'áéíóúâêîôûàèìòùäëïöüãõñç', string
+    assert_equal String::MINUSCULAS, string
   end  
 end
