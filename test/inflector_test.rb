93c3ffb5c48d171119bea3f3886b38537fe77a7b
tinorj <tinorj@ebe10805-befa-409d-9f47-33f710be3117>
2008-04-18 22:38:56 +0000

    Remoção do require 'mocha'
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brazilian-rails@127 ebe10805-befa-409d-9f47-33f710be3117

--- test/inflector_test.rb
+++ test/inflector_test.rb
@@ -4,11 +4,11 @@ class InflectorTest < Test::Unit::TestCase
   
   #general rule: add "s" to the end of the word
   def test_general_rule
-    words = {:casa => "casas",
-             :pe => "pes",
-             :saci => "sacis",
-             :carro => "carros",
-             :pneu => "pneus"
+    words = {'casa' => "casas",
+             'pe' => "pes",
+             'saci' => "sacis",
+             'carro' => "carros",
+             'pneu' => "pneus"
             }
     verify_pluralize words
     verify_singularize words
@@ -16,8 +16,8 @@ class InflectorTest < Test::Unit::TestCase
 
   #if word ends in "r" or "z", add "es"
   def test_when_word_ends_in_r_or_z
-    words = {:flor => "flores",
-             :luz => "luzes"
+    words = {'flor' => "flores",
+             'luz' => "luzes"
             }
     verify_pluralize words
     verify_singularize words
@@ -25,10 +25,10 @@ class InflectorTest < Test::Unit::TestCase
   
   #if word ends in "al", "el", "ol", "ul": trade "l" with "is"
   def test_when_word_ends_in_al_el_ol_ul
-      words = {:hospital => "hospitais",
-               :telemovel => "telemoveis",
-               :farol => "farois",
-               :azul => "azuis"
+      words = {'hospital' => "hospitais",
+               'telemovel' => "telemoveis",
+               'farol' => "farois",
+               'azul' => "azuis"
             }
     verify_pluralize words
     verify_singularize words
@@ -37,47 +37,49 @@ class InflectorTest < Test::Unit::TestCase
   
   #if word ends in "il" and has tônic accent in last syllable, trade "il" with "is"
   def test_when_word_end_in_il
-    words = {:cantil => "cantis"}
+    words = {'cantil' => "cantis"}
     verify_pluralize words
   end
 
 
   #if word ends in "m", trade "m" with "ns"
   def test_plurilize_when_word_ends_in_m
-    words = {:armazem => "armazens"}
+    words = {'armazem' => "armazens"}
     verify_pluralize words
     verify_singularize words
   end
   
   #if word ends in "s" and has one silable, trade "s" with "es"
   def test_plurilize_when_word_ends_in_s
-    words = {:portugues => "portugueses"}
+    words = {'portugues' => "portugueses"}
     verify_pluralize words
   end
   
   def test_when_word_ends_in_ao
-    words = {:portão => "portões", :portao => 'portoes', :localizacao => 'localizacoes'}
+    words = {'portão' => "portões", 
+             'portao' => 'portoes', 
+             'localizacao' => 'localizacoes'}
     verify_pluralize words
     verify_singularize words
   end
   
   def test_when_irregular_singular
-    words = {:cão => "cães",
-             :pão => "pães",
-             :mão => "mãos",
-             :alemão => "alemães",
-             :cao => 'caes',
-             :pao => 'paes',
-             :mao => 'maos',
-             :alemao => 'alemaes'
+    words = {'cão' => "cães",
+             'pão' => "pães",
+             'mão' => "mãos",
+             'alemão' => "alemães",
+             'cao' => 'caes',
+             'pao' => 'paes',
+             'mao' => 'maos',
+             'alemao' => 'alemaes'
             }
     verify_singularize words
   end
   
   
   def test_when_uncountable
-    words = {:tennis => "tennis",
-             :torax => "torax"
+    words = {'tennis' => "tennis",
+             'torax' => "torax"
             }
     verify_pluralize words
     verify_singularize words
