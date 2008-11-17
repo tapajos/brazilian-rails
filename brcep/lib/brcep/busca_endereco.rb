db82f7a43e5cc77de43e7ad716d0f5eb46212455
elomarns <elomarns@gmail.com>
2008-11-17 12:55:13 -0200

    Corrigindo o comentario do brcep que ensina a usa o BuscaEndereco.por_cep, informando um CEP como argumento (22640010), e mostrando o resultado da busca de outro CEP (22640100).

--- brcep/lib/brcep/busca_endereco.rb
+++ brcep/lib/brcep/busca_endereco.rb
@@ -8,10 +8,10 @@ require 'active_support'
 # 
 #Como fazer a busca de endereço por cep?
 #
-# BuscaEndereco.por_cep(22640010)     ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
-# BuscaEndereco.por_cep('22640010')   ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
-# BuscaEndereco.por_cep('22640-010')  ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
-# BuscaEndereco.por_cep('22.640-010') ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
+# BuscaEndereco.por_cep(22640100)     ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
+# BuscaEndereco.por_cep('22640100')   ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
+# BuscaEndereco.por_cep('22640-100')  ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
+# BuscaEndereco.por_cep('22.640-100') ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
 # BuscaEndereco.por_cep('00000000')   ==> RuntimeError 'Cep 00000000 não encontrado.'
 # 
 #Se necessário usar proxy, faça (de preferência em environment.rb):
@@ -26,7 +26,7 @@ class BuscaEndereco
   # Retorna um array com os dados de endereçamento para o cep informado ou um erro para cep inexistente.
   #
   # Exemplo:
-  #  BuscaEndereco.por_cep(22640010) ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
+  #  BuscaEndereco.por_cep(22640100) ==> ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', 22640100]
   def self.por_cep(numero)
     cep = numero.to_s.gsub(/\./, '').gsub(/\-/, '').to_i
     response = Net::HTTP.Proxy(self.proxy_addr,self.proxy_port).get_response(URI.parse("#{URL_WEB_SERVICE}#{cep}"))
