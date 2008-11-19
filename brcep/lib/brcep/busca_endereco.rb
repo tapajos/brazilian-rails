bfeb32863b9a69277f91060583e368bac0147047
elomarns <elomarns@gmail.com>
2008-11-19 14:18:15 -0200

    Adicionando um segundo web service que busca o endereco atraves do CEP. Esse outro web service (Buscar Cep) so sera utilizado caso o web service default (Bronze Business) esteja indisponivel

--- brcep/lib/brcep/busca_endereco.rb
+++ brcep/lib/brcep/busca_endereco.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'rexml/document'
 require 'active_support'
+require 'brstring'
 
 #Este recurso usa o webservice da http://www.bronzebusiness.com.br/webservices/wscep.asmx para
 #realizar as consultas de cep, então, somente funcionará para serviços com acesso a internet e
@@ -21,7 +22,12 @@ require 'active_support'
 class BuscaEndereco
   cattr_accessor :proxy_addr, :proxy_port
 
-  URL_WEB_SERVICE = 'http://www.bronzebusiness.com.br/webservices/wscep.asmx/cep?strcep=' #:nodoc:
+  URL_WEB_SERVICE_BRONZE_BUSINESS = 'http://www.bronzebusiness.com.br/webservices/wscep.asmx/cep?strcep=' #:nodoc:
+  URL_WEB_SERVICE_BUSCAR_CEP = 'http://www.buscarcep.com.br/?cep=' #:nodoc:
+
+  # Campos do XML retornado pelos web services
+  CAMPOS_XML_BRONZE_BUSINESS = %w(logradouro nome bairro UF cidade) #:nodoc:
+  CAMPOS_XML_BUSCAR_CEP = %w(tipo_logradouro logradouro bairro uf cidade) #:nodoc:
   
   # Retorna um array com os dados de endereçamento para o cep informado ou um erro para cep inexistente.
   #
@@ -30,21 +36,50 @@ class BuscaEndereco
   def self.por_cep(numero)
     raise "O CEP informado possui um formato inválido." if numero.to_s.gsub(/\./, '').gsub(/\-/, '').length != 8
 
-    cep = numero.to_s.gsub(/\./, '').gsub(/\-/, '').to_i
-    response = Net::HTTP.Proxy(self.proxy_addr,self.proxy_port).get_response(URI.parse("#{URL_WEB_SERVICE}#{cep}"))
-    
-    raise "Não foi possível obter o cep. (#{response.code} - #{response.message})" unless response.kind_of?(Net::HTTPSuccess)
-    
-    doc = REXML::Document.new(response.body)
-    
-    retorno = []
-    %w(logradouro nome bairro UF cidade).each do |e|
-      elemento = REXML::XPath.match(doc, "//#{e}").first
-      raise "Cep #{cep} não encontrado." if elemento.nil?
+    @@cep = numero.to_s.gsub(/\./, '').gsub(/\-/, '').to_i
 
-      retorno << elemento.text
-    end  
+    @@retorno = []
+
+    if web_service_da_bronze_business_esta_funcionando?
+      usar_web_service_da_bronze_business
+    elsif web_service_do_buscar_cep_esta_funcionando?
+      usar_web_service_do_buscar_cep
+    else
+      raise "A busca de endereço por CEP está indisponível no momento."
+    end
     
-    retorno << cep
+    @@retorno << @@cep
+  end
+
+  private
+  def self.web_service_da_bronze_business_esta_funcionando?
+    @@response = Net::HTTP.Proxy(self.proxy_addr, self.proxy_port).get_response(URI.parse("#{URL_WEB_SERVICE_BRONZE_BUSINESS}#{@@cep}"))
+    @@response.kind_of?(Net::HTTPSuccess)
+  end
+
+  def self.usar_web_service_da_bronze_business
+    @@doc = REXML::Document.new(@@response.body)
+    processar_xml CAMPOS_XML_BRONZE_BUSINESS
+  end
+
+  def self.web_service_do_buscar_cep_esta_funcionando?
+    @@response = Net::HTTP.Proxy(self.proxy_addr, self.proxy_port).get_response(URI.parse("#{URL_WEB_SERVICE_BUSCAR_CEP}#{@@cep}&formato=xml"))
+    @@response.kind_of?(Net::HTTPSuccess)
+  end
+
+  def self.usar_web_service_do_buscar_cep
+    @@doc = REXML::Document.new(@@response.body)
+    processar_xml CAMPOS_XML_BUSCAR_CEP
+  end
+
+  def self.processar_xml(campos_do_xml)
+    campos_do_xml.each do |e|
+      elemento = REXML::XPath.match(@@doc, "//#{e}").first
+
+      raise "Cep #{@@cep} não encontrado." if elemento.nil?
+
+      # Remove os acentos já que o Buscar Cep retorna o endereço com acento e a Bronze Business não
+      @@retorno << elemento.text.remover_acentos
+    end
   end
 end
--- brcep/test/busca_endereco_test.rb
+++ brcep/test/busca_endereco_test.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'net/http'
 require 'mocha'
 
-INVALID_ZIPS = [ 0, '0', '00', '000', '0000', '00000', '000000', '0000000' ]
+INVALID_ZIPS = [0, '0', '00', '000', '0000', '00000', '000000', '0000000']
 VALID_ZIPS = [22640100, '22640100', '22.640-100', '22.640-100']
 
 class MockSuccess < Net::HTTPSuccess
@@ -22,10 +22,13 @@ def limpa_cep(numero)
 end
 
 class BuscaEnderecoTest < Test::Unit::TestCase
-  def test_raise_witout_service
-    http_response = MockServerError.new
-    Net::HTTP.expects(:get_response).returns(http_response)
-    assert_raise RuntimeError, "Não foi possível obter o cep. (505 - HTTPServiceUnaviable)" do
+  def test_raise_without_service_on_both_web_services
+    http_error_response = MockServerError.new
+  
+    Net::HTTP.expects(:get_response).returns(http_error_response)
+    Net::HTTP.expects(:get_response).returns(http_error_response)
+  
+    assert_raise RuntimeError, "A busca de endereço por CEP está indisponível no momento." do
       BuscaEndereco.por_cep(VALID_ZIPS.first)
     end
   end
@@ -36,24 +39,49 @@ class BuscaEnderecoTest < Test::Unit::TestCase
         BuscaEndereco.por_cep(invalid_zip)
       end
     end
+  end    
+  
+  def test_valid_code_on_bronze_business
+    VALID_ZIPS.each do |valid_zip|
+      mock_get_response_from_bronze_business(limpa_cep(valid_zip))
+      assert_equal ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro',
+        limpa_cep(valid_zip)], BuscaEndereco.por_cep(valid_zip)
+    end
   end
 
-  def test_valid_code
-    VALID_ZIPS.each do |valid_zip|
-      mock_get_response(limpa_cep(valid_zip))
-      assert_equal ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro', limpa_cep(valid_zip)], BuscaEndereco.por_cep(valid_zip)
-    end
+  def test_valid_code_on_buscar_cep
+    mock_get_response_from_buscar_cep(limpa_cep(VALID_ZIPS.first))
+    assert_equal ['Avenida', 'das Americas', 'Barra da Tijuca', 'RJ', 'Rio de Janeiro',
+      limpa_cep(VALID_ZIPS.first)], BuscaEndereco.por_cep(VALID_ZIPS.first)
+  end
+
+  def test_should_return_the_same_address_on_both_web_services
+    mock_get_response_from_bronze_business(limpa_cep(VALID_ZIPS.first))
+    mock_get_response_from_buscar_cep(limpa_cep(VALID_ZIPS.first))
+    assert_equal  BuscaEndereco.por_cep(VALID_ZIPS.first), BuscaEndereco.por_cep(VALID_ZIPS.first)
   end
 
   private
 
-  def mock_get_response(zip_number)
-    xml_data = xml_data_from zip_name(zip_number)
+  def mock_get_response_from_bronze_business(zip_number)
+    xml_data = xml_data_from zip_name(zip_number, "bronze_business")
 
     http_response = MockSuccess.new
     http_response.expects(:body).returns(xml_data)
 
     Net::HTTP.expects(:get_response).returns(http_response)
+  end
+
+  def mock_get_response_from_buscar_cep(zip_number)
+    xml_data = xml_data_from zip_name(zip_number, "buscar_cep")
+
+    http_success_response = MockSuccess.new
+    http_success_response.expects(:body).returns(xml_data)
+
+    Net::HTTP.expects(:get_response).returns(http_success_response)
+
+    http_error_response = MockServerError.new
+    Net::HTTP.expects(:get_response).returns(http_error_response)
   end
 
   def xml_data_from xml_mock
@@ -68,7 +96,7 @@ class BuscaEnderecoTest < Test::Unit::TestCase
     xml_data
   end
 
-  def zip_name(nome)
-    File.join(File.dirname(__FILE__), 'mocks', "zip_#{nome}.xml")
+  def zip_name(nome, web_service)
+    File.join(File.dirname(__FILE__), 'mocks', "zip_#{nome}_#{web_service}.xml")
   end
 end
