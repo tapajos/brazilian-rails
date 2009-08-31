1ebb82ac2efad832a8b0362f0af0075b55c7856e
Fernando Luizao <fernandoluizao@gmail.com>
2009-08-31 20:53:42 -0300

    ruby 1.9 fix: alterado to_date para nao usar ParseDate, que foi removido

--- brdata/lib/brdata/date_portuguese.rb
+++ brdata/lib/brdata/date_portuguese.rb
@@ -7,7 +7,7 @@ module ActiveSupport::CoreExtensions::String::Conversions
     if /(\d{1,2})\W(\d{1,2})\W(\d{4})/ =~ self
       ::Date.new($3.to_i, $2.to_i, $1.to_i)
     else
-      ::Date.new(*ParseDate.parsedate(self)[0..2])
+      ::Date.new(*::Date._parse(self, false).values_at(:year, :mon, :mday))
     end
   end
 end
