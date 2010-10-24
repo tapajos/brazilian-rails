619bee568717a3131189b14f69fd79d8a5e53e94
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-10-24 12:47:05 -0200

    Corrigido require no brazilian-rails

--- lib/brazilian-rails.rb
+++ lib/brazilian-rails.rb
@@ -1,6 +1,6 @@
 PROJECTS = %w(brdinheiro brcep brdata brhelper brnumeros brstring brcpfcnpj)
 
 PROJECTS.each do |project|
-  require File.dirname(__FILE__) + "/../#{project}/lib/#{project}"
+  require project
 end
 
