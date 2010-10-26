df9dbeb2bd942b67aa7c6121913217b19ac21d85
stJhimy <stjhimy@gmail.com>
2010-10-26 11:52:26 -0200

    brI18n aceita bloco de configuração, pt-BR.yml renomeado para rails.pt-BR.yml

--- brI18n/lib/brI18n.rb
+++ brI18n/lib/brI18n.rb
@@ -1,13 +1,30 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 %w(version).each {|req| require File.dirname(__FILE__) + "/brI18n/#{req}"}
 
 module BrI18n
   
-  LOCALES = File.expand_path(File.dirname(__FILE__) + "/locales")
-  
+  LOCALES_PATH = File.expand_path(File.dirname(__FILE__) + "/locales")
+
+  def self.setup
+    yield self
+  end
+
+  private
+  def self.ativar_traducoes
+    I18n.default_locale = 'pt-BR'
+  end
+
+  def self.ativar_locales(*args)
+    if args.empty?
+      I18n.load_path += Dir.glob("#{BrI18n::LOCALES_PATH}/*.pt-BR.yml")
+    else
+      args.each do |arg|
+        puts args
+        I18n.load_path << "#{BrI18n::LOCALES_PATH}/#{arg}.pt-BR.yml"
+      end
+    end
+  end
+
 end
 
-I18n.load_path += Dir.glob("#{BrI18n::LOCALES}/*")
-I18n.default_locale = 'pt-BR'
+
+
--- brI18n/test/brI18n_test.rb
+++ brI18n/test/brI18n_test.rb
@@ -2,13 +2,48 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), "test_helper.rb")
 class BrI18nTest < ActiveSupport::TestCase
 
+
+  def setup
+    I18n.load_path = []
+    BrI18n.setup do |config|
+      config.ativar_traducoes
+    end
+  end
+
+  test "brI18n deve aceitar um bloco de configuração" do
+    BrI18n.setup do |config|
+      assert_equal BrI18n, config
+    end
+  end
+
   test "Verifica se default_locale é igual a pt-BR" do
     assert_equal I18n.default_locale.to_s, "pt-BR"
   end
 
-  test "Verifica se I18n.load_path inclui os arquivos de tradução do brI18n" do
-    Dir.glob("#{BrI18n::LOCALES}/*").each do |file|
+  test "ativar_locales deve incluir todos arquivos se chamado sem parâmetros" do
+    BrI18n.setup do |config|
+      config.ativar_locales
+    end
+
+    Dir.glob("#{BrI18n::LOCALES_PATH}/*").each do |file|
       assert I18n.load_path.include?(file)
     end
   end
+
+  test "ativar_locales deve incluir apenas arquivos selecionados se chamado com parâmetros" do
+    BrI18n.setup do |config|
+      config.ativar_locales :rails
+    end
+    assert_equal I18n.load_path.count, 1
+    assert_equal I18n.load_path.first, "#{BrI18n::LOCALES_PATH}/rails.pt-BR.yml"
+
+    I18n.load_path = []
+    BrI18n.setup do |config|
+      config.ativar_locales :devise
+    end
+
+    assert_equal I18n.load_path.count, 1
+    assert_equal I18n.load_path.first, "#{BrI18n::LOCALES_PATH}/devise.pt-BR.yml"
+  end
+
 end
