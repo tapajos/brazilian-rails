295c035bfd08cdad6ddf1a4d2fe82b49a2080cc3
stJhimy <stjhimy@gmail.com>
2010-10-29 09:36:35 -0200

    Updated README for brI18n and put the generators in separated folders, just need to write the USAGE for each one now

--- brI18n/lib/generators/br_i18n/locales/locales_generator.rb
+++ brI18n/lib/generators/br_i18n/locales/locales_generator.rb
@@ -2,11 +2,11 @@ require "rails/generators"
 module BrI18n
   module Generators
     class LocalesGenerator < Rails::Generators::Base
-      source_root File.join(File.dirname(File.expand_path(__FILE__)), "../../locales/")  
+      source_root File.join(File.dirname(File.expand_path(__FILE__)), "../../../locales/")  
       desc = "Efetua uma cópia dos locales para a pasta do projeto, sendo assim possível criar suas customizações"
       
       def copy_files
-        Dir.glob(File.join(File.dirname(File.expand_path(__FILE__)), "../../locales/*.yml")).each do |file|
+        Dir.glob(File.join(File.dirname(File.expand_path(__FILE__)), "../../../locales/*.yml")).each do |file|
           copy_file file, "config/locales/#{File.split(file).last}"
         end
       end
--- brI18n/test/test_helper.rb
+++ brI18n/test/test_helper.rb
@@ -5,5 +5,5 @@ require "rails"
 require 'rails/test_help'
 
 require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/brI18n.rb")
-require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/generators/br_i18n/locales_generator.rb")
-require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/generators/br_i18n/install_generator.rb")
+require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/generators/br_i18n/locales/locales_generator.rb")
+require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/generators/br_i18n/install/install_generator.rb")
