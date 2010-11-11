7148fbb8587dd66918360df7be89ff55cca4c3ea
stJhimy <stjhimy@gmail.com>
2010-11-11 09:56:00 -0200

    Fixing typo

--- brnumeros/lib/brnumeros/number_portuguese.rb
+++ brnumeros/lib/brnumeros/number_portuguese.rb
@@ -1,45 +1,15 @@
 # encoding: UTF-8
 module Extenso 
-  @@unidade = {
-    0 => "zero",  
-    1 => "um",  
-    2 => "dois",  
-    3 => "três",  
-    4 => "quatro",  
-    5 => "cinco",  
-    6 => "seis",  
-    7 => "sete",  
-    8 => "oito",  
-    9 => "nove" 
-  }
-  @@dezena = {
-    10 => "dez",  
-    11 => "onze",  
-    12 => "doze",  
-    13 => "treze",  
-    14 => "quatorze",  
-    15 => "quinze",  
-    16 => "dezesseis",  
-    17 => "dezessete",  
-    18 => "dezoito",  
-    19 => "dezenove",  
-    20 => "vinte",  
-    30 => "trinta",  
-    40 => "quarenta",  
-    50 => "cinquenta",  
-    60 => "sessenta",  
-    70 => "setenta",  
-    80 => "oitenta",  
-    90 => "noventa"  }
-  @@centena = {100 => "cento",  
-    200 => "duzentos",  
-    300 => "trezentos",  
-    400 => "quatrocentos",  
-    500 => "quinhentos",  
-    600 => "seiscentos",  
-    700 => "setecentos",  
-    800 => "oitocentos",  
-    900 => "novecentos"  }
+  @@unidade = {0 => "zero", 1 => "um", 2 => "dois", 3 => "três", 4 => "quatro", 5 => "cinco",
+               6 => "seis", 7 => "sete", 8 => "oito", 9 => "nove"}
+
+  @@dezena = {10 => "dez", 11 => "onze", 12 => "doze", 13 => "treze", 14 => "quatorze", 15 => "quinze",
+              16 => "dezesseis", 17 => "dezessete", 18 => "dezoito", 19 => "dezenove", 20 => "vinte",
+              30 => "trinta", 40 => "quarenta", 50 => "cinquenta", 60 => "sessenta", 70 => "setenta",
+              80 => "oitenta", 90 => "noventa"}
+
+  @@centena = {100 => "cento", 200 => "duzentos", 300 => "trezentos", 400 => "quatrocentos", 500 => "quinhentos",
+               600 => "seiscentos", 700 => "setecentos", 800 => "oitocentos", 900 => "novecentos"  }
   
   # Escreve o numero por extenso.
   # 
