78bd76bfed441558c0db6dc5d81ec604bdddf3c6
stJhimy <stjhimy@gmail.com>
2010-11-11 10:54:52 -0200

    Fixed typo and require

--- brstring/lib/brstring.rb
+++ brstring/lib/brstring.rb
@@ -2,7 +2,7 @@ require "brstring/version"
 
 module BrString
 
- require "brstring/string_portuguese" 
+  require "brstring/string_portuguese" 
 
 end
 
--- brstring/test/test_helper.rb
+++ brstring/test/test_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] = "test"
 require 'test/unit'
 require 'rubygems'
 require "rails"
-require "rails/generators"
 require 'rails/test_help'
 
 require "brstring"
