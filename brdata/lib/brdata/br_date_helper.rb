ab76a810c0629c56f924acfb07ea8c1616fd5109
Marcelo Junior <marcelo.machado.jr@gmail.com>
2014-12-09 01:35:32 -0200

    O parametro "options = {}" foi adicionado a partir do ActionPack 3.2.13

--- brdata/lib/brdata/br_date_helper.rb
+++ brdata/lib/brdata/br_date_helper.rb
@@ -2,7 +2,7 @@
 module ActionView::Helpers::DateHelper
   # Traduz o método distance_of_time_in_words para retornar esse valor em português
   #
-  def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false)
+  def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, options = {})
     from_time = from_time.to_time if from_time.respond_to?(:to_time)
     to_time = to_time.to_time if to_time.respond_to?(:to_time)
     distance_in_minutes = (((to_time - from_time).abs)/60).round
